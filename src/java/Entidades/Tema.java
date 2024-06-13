ca9a64a200585adee0583dd1702b894608f8ce2b
guillermo-gordon18-2000 <guillermo18zgordon@gmail.com>
2024-06-13 17:34:50 -0500

    4-12-2022 Logica para los temas dentro del juego

---
+++ src/java/Entidades/Tema.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author lueun
+ */
+public class Tema {
+    private String cod;
+    private String nombre;   
+    private String image;
+    private String content;
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }        
+}
